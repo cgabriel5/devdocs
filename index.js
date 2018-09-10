f4738ee7fdee3c76eada12ba0387ccaaf9d39d5d
cgabriel5 <carlosgabriel71@yahoo.com>
2018-09-10 11:17:01 -0700

    chore: Update the timeago and timelong functions
    - Update both functions to their latest versions.
    - Rename the timelong function to timedate.

--- index.js
+++ index.js
@@ -419,6 +419,7 @@ var regexp_index = function(string, regexp, startindex) {
  * @param  {number} timestamp - The timestamp in milliseconds (not UNIX).
  * @param  {boolean} format12 - Format in 12 hour format.
  * @param  {string} delimiter - Delimiter character.
+ * @param  {function} cb - Optional custom format function.
  * @return {string} - The timestamp pretty format.
  *
  * @resource [https://stackoverflow.com/a/6078873]
@@ -426,7 +427,12 @@ var regexp_index = function(string, regexp, startindex) {
  * @resource [https://stackoverflow.com/a/5971324]
  * @resource [https://www.w3schools.com/js/js_date_methods.asp]
  */
-var timelong = function(timestamp, format12, delimiter) {
+var timedate = function(timestamp, format12, delimiter, cb) {
+	// Timestamp must be a number.
+	if (!timestamp || typeof timestamp !== "number") {
+		return undefined;
+	}
+
 	// Default the delimiter to nothing.
 	delimiter = delimiter || " ";
 	if (delimiter.trim() !== "") {
@@ -460,10 +466,26 @@ var timelong = function(timestamp, format12, delimiter) {
 	hour = hour > 12 && format12 ? hour - 12 : hour;
 	hour = hour === 0 ? 12 : hour;
 
-	// Format the time in the following format:
-	return `${year}-${month}-${day}${delimiter}${prefix_zero(
-		hour
-	)}:${prefix_zero(min)}:${prefix_zero(sec)}`;
+	// Zero prefix time vars.
+	hour = prefix_zero(hour);
+	min = prefix_zero(min);
+	sec = prefix_zero(sec);
+
+	// If a custom format function is supplied use it.
+	if (cb && typeof cb === "function") {
+		return cb.call({
+			year: year,
+			month: month,
+			day: day,
+			hour: hour,
+			min: min,
+			sec: sec
+		});
+	}
+
+	// Format the time in the following format when no custom
+	// format function is supplied:
+	return `${year}-${month}-${day}${delimiter}${hour}:${min}:${sec}`;
 };
 
 /**
@@ -1379,7 +1401,7 @@ versions.forEach(function(vdata) {
 						var mtime = Math.round(stats.mtimeMs);
 
 						// Build the timeago HTML.
-						let timeago_html = `<div class="mtime-cont"><div><span class="bold"><i class="fas fa-edit"></i> Last update:</span> <span class="none mtime-ts animate-fadein" data-ts="${mtime}"></span> <span class="mtime-ts-long">(${timelong(
+						let timeago_html = `<div class="mtime-cont"><div><span class="bold"><i class="fas fa-edit"></i> Last update:</span> <span class="none mtime-ts animate-fadein" data-ts="${mtime}"></span> <span class="mtime-ts-long">(${timedate(
 							mtime
 						)})</span></div></div>`;
 
